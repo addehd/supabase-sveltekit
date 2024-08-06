3edc52a8323b3ea191e825eb43c9c950dcdf4276
addehd <hej@adolfo.se>
2024-08-06 20:40:45 +0200

    supabase auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,21 @@
-<script>
+<script lang="ts">
+  import { supabase } from '$lib/supabase'
+  import { invalidate } from '$app/navigation';
+  import { onMount } from 'svelte'
   import "../app.css";
-</script>
-<slot />
+  
+  onMount(() => {
+    const {
+      data: { subscription }
+    } = supabase.auth.onAuthStateChange(() => { 
+      invalidate('supabase:auth')
+    })
+    return () => {
+      subscription.unsubscribe()
+    }
+  })
+  </script>
+  
+  <main class="dark:bg-gray-900 min-h-[100vh]">
+    <slot />
+  </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-import { supabase } from '$lib/supabaseClient';
+import { supabase } from '$lib/supabase';
 
 let email = ''
 let password = ''
