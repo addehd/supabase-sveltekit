35bba7ee04c02cda171e3f6a7c72dce91634a317
addehd <hej@adolfo.se>
2024-08-17 23:59:03 +0200

    inspector plugin

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess()],
+
+	// kit: {
+	// 	adapter: adapter()
+	// },
+  vitePlugin: {
+    experimental: {
+      inspector: true
+    }
+  }
 };
+
 export default config;
