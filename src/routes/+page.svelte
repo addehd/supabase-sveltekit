3b0a2698e8677b2e8f256afb1c5c1aca677de55c
addehd <hej@adolfo.se>
2024-09-29 21:59:33 +0200

    loading..

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,9 +29,9 @@
                       <a href="/" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div> -->
                   <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log in to your account</button>
-                  <p class="text-sm font-light text-center text-gray-500 dark:text-gray-300">
+                  <!-- <p class="text-sm font-light text-center text-gray-500 dark:text-gray-300">
                         <a href="/" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Don't have an account?</a>
-                  </p>
+                  </p> -->
               </form>
           </div>
       </div>
--- src/routes/rum/[slug]/+page.svelte
+++ src/routes/rum/[slug]/+page.svelte
@@ -1,12 +1,16 @@
 <script>
   import { onMount } from 'svelte';
   import { createScene } from '$lib/main';
+  import Loading from '/Users/addehd/Documents/dev/coetiv/hangaren/arkivet/src/lib/components/Loading.svelte';
+
+
   
   export let data;
 
   console.log('data', data);
   let imageUrl = data.artworks[0].image_url;
   let el;
+  let showDiv = true;  // Initially show the div
 
   if (!Array.isArray(data.artworks)) {
     console.error('data.artworks is not an array');
@@ -16,10 +20,19 @@
     if (Array.isArray(data.artworks)) {
       createScene(el, data.artworks);
     }
+    
+    setTimeout(() => {
+      showDiv = false;
+    }, 5500);
+
+    const svg = document.querySelector('svg');
+    if (svg) {
+      svg.classList.add('active');
+    }
   });
 </script>
 
-<header class="bg-purple-400 fixed bottom-0 w-full  p-4">
+<header class="bg-purple-400 fixed bottom-0 w-full  p-4 z-50">
   <nav class="flex space-x-[20rem] justify-around">
     <div class="text-white font-bold text-xl left-20">
       Hangaren/CFUK 
@@ -28,17 +41,15 @@
     <div class="text-white font-bold text-xl">
       PROTOTYP - GALLERIHELG
     </div>
-    <!-- <ul class="flex space-x-4 items-center">
-      <li><a href="/" class="text-white hover:text-gray-200">home</a></li>
-      <li><a href="/exhibitions" class="text-white hover:text-gray-200">exhibitions</a></li>
-      <li><a href="/rum" class="text-white hover:text-gray-200">about</a></li>
-      <li>
-        <button on:click={playBirdsSound} class="ml-20 px-3 py-1 bg-green-500 text-white rounded hover:bg-green-600">
-          Play
-        </button>
-      </li>
-    </ul> -->
   </nav>
 </header>
-    
-<canvas bind:this={el} />
+
+{#if showDiv}
+  <div class="fixed inset-0 bg-black flex items-center justify-center z-50">
+    <div class="w-[20rem] h-[10rem]">
+      <Loading />
+    </div>
+  </div>
+{/if}
+
+<canvas class="w-full h-full fixed top-0 left-0" bind:this={el} />
