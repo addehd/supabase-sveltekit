e9084c31f6ac6e2f40b9644702e93889180cc200
addehd <hej@adolfo.se>
2025-01-10 14:44:42 +0100

    clean ux

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -30,20 +30,70 @@
 	function playAndLoad() {
 		$videoIsPlaying = true;
 	}
+
+	let isFilledIconVisible = false;
+
+function showFilledIcon() {
+	isFilledIconVisible = true;
+}
+
+function showOutlineIcon() {
+	isFilledIconVisible = false;
+}
 </script>
 
-<header class="fixed backdrop-blur-lg top-0 w-full z-50 flex items-center justify-between border-b-[1px] border-white/20">
-	<nav class="flex space-x-[20rem] justify-between w-full items-center">
-		<div></div>
-		<div class="flex space-x-2">
-			<img src="/logo.svg" alt="logo" class="my-5 h-[3rem] text-center" />
-		</div>
-		<div class="text-white font-bold text-xl py-7 left-0">
-			<!-- <a class="px-11 flex items-center gap-1" href="/stappen/34">
-				CFUK Centrum för konst och kultur
-			</a> -->
-		</div>
-	</nav>
+<header
+  class="fixed backdrop-blur-lg top-0 w-full z-50 flex items-center justify-between border-b-[1px] border-white/20">
+  <nav class="flex  space-x-[20rem] justify-between w-full items-center">
+    <div>
+      <div on:mouseenter={showFilledIcon} on:mouseleave={showOutlineIcon}>
+        <svg
+          id="filled-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="icon icon-tabler icons-tabler-filled icon-tabler-info-circle text-white"
+          class:hidden={!isFilledIconVisible}
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm0 9h-1l-.117 .007a1 1 0 0 0 0 1.986l.117 .007v3l.007 .117a1 1 0 0 0 .876 .876l.117 .007h1l.117 -.007a1 1 0 0 0 .876 -.876l.007 -.117l-.007 -.117a1 1 0 0 0 -.764 -.857l-.112 -.02l-.117 -.006v-3l-.007 -.117a1 1 0 0 0 -.876 -.876l-.117 -.007zm.01 -3l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007z" />
+        </svg>
+        <svg
+          id="outline-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="icon icon-tabler icons-tabler-outline icon-tabler-info-circle text-white"
+          class:hidden={isFilledIconVisible}
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+          <path d="M12 9h.01" />
+          <path d="M11 12h1v4h1" />
+        </svg>
+      </div>
+    </div>
+    <div class="flex space-x-2">
+      <!-- <p class="text-white marquee">{ $description }</p> -->
+      <img src="/logo.svg" alt="logo" class="my-5 h-[3rem] text-center" />
+    </div>
+    <div class="text-white font-bold text-xl py-7 left-0">
+      <!-- <a class="px-11 flex items-center gap-1" href="/stappen/34">
+        CFUK Centrum för konst och kultur
+        <!-- <svg class="w-9 h-9" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path stroke="currentColor" fill="none" stroke-width="1.5" d="M14 6l6 6-6 6" />
+        </svg> 
+      </a> -->
+    </div>
+  </nav>
 </header>
 
 <main class="dark:bg-gray-900 min-h-[100vh]">
--- src/routes/rum/[slug]/+page.svelte
+++ src/routes/rum/[slug]/+page.svelte
@@ -26,14 +26,7 @@
   let audio;
   let iframeElement: HTMLIFrameElement;
 
-  $: {
-    console.log('audioSource: page', $audioSource);
-    if (audio) {
-      audio.src = $audioSource;
-    }
-  }
-
-  // log audioSource when it changes
+  $: { if (audio) { audio.src = $audioSource; } }
 
   function playAndLoad() {
     loadSmileyFaceWrapper();
@@ -41,10 +34,6 @@
       audio.play();
     }
   }
-
-  // function toggleVideo() {
-  //   $videoIsPlaying = !$videoIsPlaying;
-  // }
 
   onMount(() => {
     createScene(el, data.artworks);
@@ -75,68 +64,9 @@
   function toggleVideo() {
     $videoIsPlaying = !$videoIsPlaying;
   }
+
 </script>
 
-<header
-  class="fixed backdrop-blur-lg top-0 w-full z-50 flex items-center justify-between border-b-[1px] border-white/20">
-  <nav class="flex  space-x-[20rem] justify-between w-full items-center">
-    <div></div>
-    <div class="flex space-x-2">
-      <!-- <p class="text-white marquee">{ $description }</p> -->
-      <img src="/logo.svg" alt="logo" class="my-5 h-[3rem] text-center" />
-    </div>
-    <div class="text-white font-bold text-xl py-7 left-0">
-      <!-- <a class="px-11 flex items-center gap-1" href="/stappen/34">
-        CFUK Centrum för konst och kultur
-        <!-- <svg class="w-9 h-9" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path stroke="currentColor" fill="none" stroke-width="1.5" d="M14 6l6 6-6 6" />
-        </svg> 
-      </a> -->
-    </div>
-  </nav>
-</header>
-
-<!-- bottom nav -->
-<div class="fixed backdrop-blur-lg bottom-0 w-full z-50 flex items-center justify-between border-t-[1px] border-white/20">
-  <nav class="flex  space-x-[20rem] justify-between w-full items-center">
-   
-    <!-- <label on:click={toggleVideo} class="w-9 h-10 cursor-pointer flex flex-col items-center justify-center">
-      <input class="hidden peer" type="checkbox" />
-      <div
-        class="w-[50%] h-[2px] bg-white rounded-sm transition-all duration-300 origin-center rotate-90 -translate-x-[0.3rem] translate-y-[0.1rem] peer-checked:translate-y-[0.1rem]">
-      </div>
-      <div
-        class="w-[50%] h-[2px] bg-white rounded-md transition-all duration-300 origin-center rotate-90 translate-x-[0.3rem] -translate-y-[0.05rem] peer-checked:rotate-[-30deg] peer-checked:translate-y-[0.22rem] peer-checked:translate-x-[0.15rem]">
-      </div>
-      <div
-        class="w-[50%] h-[2px] bg-white rounded-md transition-all duration-300 origin-center rotate-90 translate-x-[0.3rem] -translate-y-[0.16rem] peer-checked:rotate-[30deg] peer-checked:translate-y-[-0.4rem] peer-checked:translate-x-[0.15rem]">
-      </div>
-    </label> -->
-
-    <div class="flex space-x-2 ml-32">
-      <button class="text-white font-bold text-xl h-[3rem] pl-44 hover:cursor-pointer smile" on:click={playAndLoad}
-        on:keydown={(e)=> e.key === 'Enter' && playAndLoad()}>
-        <svg class="h-[3rem] inline-block ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <circle fill="white" cx="36" cy="40.2" r="5" />
-          <circle fill="white" cx="64" cy="40.2" r="5" />
-          <path fill="white"
-            d="M50 10c-22.1 0-40 17.9-40 40s17.9 40 40 40 40-17.9 40-40-17.9-40-40-40zm0 74.4C31 84.4 15.6 69 15.6 50S31 15.6 50 15.6 84.4 31 84.4 50 69 84.4 50 84.4z" />
-          <path fill="white"
-            d="M63.5 59.5c-7.4 7.4-19.6 7.4-27 0-1.1-1.1-2.9-1.1-3.9 0-1.1 1.1-1.1 2.9 0 3.9 4.8 4.8 11.1 7.2 17.5 7.2s12.6-2.4 17.5-7.2c1.1-1.1 1.1-2.9 0-3.9-1.2-1.1-3-1.1-4.1 0z" />
-        </svg>
-      </button>
-
-    </div>
-    <div class="text-white bg-green-600 font-bold text-xl py-7 left-0">
-      <a class="px-11 flex items-center gap-1" href="/stappen/34">
-        till Stäppen
-        <svg class="w-9 h-9" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path stroke="currentColor" fill="none" stroke-width="1.5" d="M14 6l6 6-6 6" />
-        </svg>
-      </a>
-    </div>
-  </nav>
-</div>
 
 <canvas class="w-full h-full fixed top-0 left-0" bind:this={el} on:click|preventDefault|stopPropagation={handleClick} />
 
@@ -167,5 +97,13 @@
     to {
       transform: translateX(-100%);
     }
+  }
+
+  .icon {
+    transition: opacity 0.3s ease;
+  }
+  .hidden {
+    opacity: 0;
+    pointer-events: none;
   }
 </style>
